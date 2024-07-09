94fdbeec6651ddbecbd226b94fd44fd9953bce43
Johnson Epo <145839868+johnsonepo@users.noreply.github.com>
2024-07-09 08:55:35 +0400

    Delete markyee-backend directory

--- markyee-backend/protected/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'connectionString' => 'mysql:host=127.0.0.1;dbname=mtest',
-    'emulatePrepare' => true,
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-);
